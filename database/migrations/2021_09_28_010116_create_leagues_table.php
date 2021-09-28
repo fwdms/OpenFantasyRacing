62d736649acbbd13ffc925491a123a8aedcfcf00
Karl Murray <kmurray@foxworldtravel.com>
2021-09-27 20:07:18 -0500

    Added some Models and Migrations

---
+++ database/migrations/2021_09_28_010116_create_leagues_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLeaguesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('leagues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('franchise_id');
+            $table->unsignedBigInteger('league_owner_id');
+            $table->foreign('franchise_id')->references('id')->on('franchises');
+            $table->foreign('league_owner_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('leagues');
+    }
+}
