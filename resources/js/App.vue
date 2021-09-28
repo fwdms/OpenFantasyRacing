bc655e743b8033364c745ec34f3045e009211e63
Karl Murray <kmurray@foxworldtravel.com>
2021-09-27 19:54:36 -0500

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="bg-grey-100">
+        <h1 class="text-3xl">Fantasy F1</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
