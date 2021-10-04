0d369cc3130b30985e6c78fa0f6374054f6cc778
Karl Murray <kmurray@foxworldtravel.com>
2021-10-04 17:53:17 -0500

    Started working on Factories, and Tests

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,8 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
+            'wins' => $this->faker->numberBetween(1, 100),
+            'profile_image' => $this->faker->imageUrl,
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -11,8 +11,8 @@ class ExampleTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
+    public function example()
     {
-        $this->assertTrue(true);
+        //
     }
 }
