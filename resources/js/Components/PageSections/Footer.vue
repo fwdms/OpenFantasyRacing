d44576d8f820d6c28e9cccf7713eaa8fc72f2b7b
Karl Murray <kmurray@foxworldtravel.com>
2022-04-19 10:33:28 -0500

    Maybe?

--- resources/js/Components/PageSections/Footer.vue
+++ resources/js/Components/PageSections/Footer.vue
@@ -20,30 +20,30 @@
 </template>
 
 <script setup>
-import { defineComponent, h } from 'vue'
+  import { defineComponent, h } from 'vue';
 
-const navigation = {
-  main: [
-    { name: 'Docs / Roadmap', href: 'https://fwdms.plutio.com/p/zzqngybr3ztyqnmgb?board=xXn9kQmf9F5iep83Q', target: '_blank' },
-    { name: 'Report a Bug', href: '/bug-report', target: '_blank' },
-    { name: 'Request a New Feature', href: '/feature-request', target: '_blank' },
-    // { name: 'Press', href: '#' },
-    // { name: 'Accessibility', href: '#' },
-    // { name: 'Partners', href: '#' },
-  ],
-  social: [
-    {
-      name: 'Twitter',
-      href: 'https://twitter.com/karlcodeswell',
-      icon: defineComponent({
-        render: () =>
-          h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
-            h('path', {
-              d: 'M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84',
-            }),
-          ]),
-      }),
-    },
-  ],
-}
+  const navigation = {
+    main: [
+      { name: 'Docs / Roadmap', href: 'https://fwdms.plutio.com/p/zzqngybr3ztyqnmgb?board=xXn9kQmf9F5iep83Q', target: '_blank' },
+      { name: 'Report a Bug', href: '/bug-report', target: '_blank' },
+      { name: 'Request a New Feature', href: '/feature-request', target: '_blank' },
+      // { name: 'Press', href: '#' },
+      // { name: 'Accessibility', href: '#' },
+      // { name: 'Partners', href: '#' },
+    ],
+    social: [
+      {
+        name: 'Twitter',
+        href: 'https://twitter.com/karlcodeswell',
+        icon: defineComponent({
+          render: () =>
+            h('svg', { fill: 'currentColor', viewBox: '0 0 24 24' }, [
+              h('path', {
+                d: 'M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84',
+              }),
+            ]),
+        }),
+      },
+    ],
+  }
 </script>
