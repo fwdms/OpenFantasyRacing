580bce1c2579dfdb773361fedecd05c5d149c5fc
Karl Murray <kmurray@foxworldtravel.com>
2022-04-19 12:05:46 -0500

    made the font load in sooner.

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,16 +4,16 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- OpenFantasyRacing Font -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
+
         <!-- Favicon -->
         <link rel="shortcut icon" href="{{ asset('favicon.png')}}" type="image/x-icon">
 
         <!-- Inertia Page Title -->
         <title inertia>{{ config('app.name') }}</title>
-
-        <!-- OpenFantasyRacing Font -->
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
 
         <!-- Fonts -->
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
@@ -25,7 +25,7 @@
         @routes
         <script src="{{ mix('js/app.js') }}" defer></script>
     </head>
-    
+
     <body class="font-sans antialiased">
         @inertia
     </body>
