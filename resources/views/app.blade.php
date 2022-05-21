93648950e0bf9aeb0f8152e45728b59c9e22a8e3
Karl Murray <kmurray@foxworldtravel.com>
2022-05-20 22:15:22 -0500

    Added Share Meta

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta property="og:title" content="OpenFantasyRacing.com">
+        <meta property="og:image" content="{{ asset('favicon.png') }}">
 
         <!-- OpenFantasyRacing Font -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -10,7 +12,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="{{ asset('favicon.png')}}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
 
         <!-- Inertia Page Title -->
         <title inertia>{{ config('app.name') }}</title>
