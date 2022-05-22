e6ed8fec0263528b1f042e5f98f3384426324c94
Karl Murray <kmurray@foxworldtravel.com>
2022-05-22 16:57:29 -0500

    Constructors logo support

--- resources/js/Components/Lists/ConstructorsGridList.vue
+++ resources/js/Components/Lists/ConstructorsGridList.vue
@@ -4,6 +4,7 @@
       <div class="w-full flex items-center justify-between p-6 space-x-6">
         <div class="flex-1 truncate">
           <div class="flex items-center space-x-3">
+            <img v-if="constructor.logo != null" class="w-10 h-10 flex-shrink-0" :src="constructor.logo" alt="" />
             <h3 class="text-gray-900 text-sm font-medium truncate">{{ constructor.name }}</h3>
             <!-- <span class="flex-shrink-0 inline-block px-2 py-0.5 text-green-800 text-xs font-medium bg-green-100 rounded-full">{{ driver.constructor.name }}</span> -->
           </div>
@@ -11,7 +12,6 @@
         </div>
         <div class="flex items-center space-x-3">
           <!-- <p class="font-bold text-xl">{{ driver.number }}</p> -->
-          <!-- <img v-if="driver.profile_image != null" class="w-10 h-10 bg-gray-300 rounded-full flex-shrink-0" :src="driver.profile_image" alt="" /> -->
         </div>
       </div>
       <div>
