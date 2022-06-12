a3db4c10813be232543dcb0e94cd94eeb6d13b69
Karl Murray <kmurray@foxworldtravel.com>
2022-06-12 13:45:19 -0500

    Solved round numbers issue.

--- resources/js/Components/Lists/ResultsTable.vue
+++ resources/js/Components/Lists/ResultsTable.vue
@@ -33,9 +33,9 @@
 							</tr>
 						</thead>
 						<tbody class="divide-y divide-gray-200 bg-white">
-							<tr v-for="(result, index) in results" :key="result.id">
+							<tr v-for="result in results" :key="result.id">
                 <td class="whitespace-nowrap py-4 w-96 pl-4 pr-3 text-center text-sm font-medium text-gray-900 sm:pl-6">
-									{{ index + 1 }}
+									{{ result.round_number }}
 								</td>	
 								<td class="whitespace-nowrap py-4 w-96 pl-4 pr-3 text-center text-sm font-medium text-gray-900 sm:pl-6">
 									<Link :href="'/franchise/' + franchise.slug + '/events/' + result.race_id" class="text-orange-600">
