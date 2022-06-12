00cf71ad76a34f677e0885e4f67fc9bb11ab4982
Karl Murray <kmurray@foxworldtravel.com>
2022-06-12 09:47:04 -0500

    Center it.

--- resources/js/Components/Lists/EventsList.vue
+++ resources/js/Components/Lists/EventsList.vue
@@ -8,23 +8,23 @@
               <thead class="bg-gray-50">
                 <tr>
                   <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-center">Round</th>
-                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Track</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Country</th>
-                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Date</th>
+                  <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 text-center">Name</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-center">Track</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-center">Country</th>
+                  <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 text-center">Date</th>
                 </tr>
               </thead>
               <tbody class="divide-y divide-gray-200 bg-white">
                 <tr v-for="(event, index) in events" :key="event.id">
                   <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{{ index + 1 }}</td>
-                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6 text-center">
                     <Link :href="currentUrl + '/events/' + event.id" class="text-orange-600">
                       {{ event.name }}
                     </Link>
                   </td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ event.track[0].name }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ event.track[0].location }}</td>
-                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{ event.date }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{{ event.track[0].name }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{{ event.track[0].location }}</td>
+                  <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500 text-center">{{ event.date }}</td>
                 </tr>
               </tbody>
             </table>
