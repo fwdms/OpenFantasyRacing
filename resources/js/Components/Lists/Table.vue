6da3719c3a2b8a23eb2b9ae20b9acb647b71aa88
Karl Murray <kmurray@foxworldtravel.com>
2022-07-09 20:53:34 -0500

    Fixed a margin issue on the tables

--- resources/js/Components/Lists/Table.vue
+++ resources/js/Components/Lists/Table.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="bg-white my-8 mx-8 pt-4 pb-2 md:px-8 rounded">
+  <div class="bg-white my-8 mx-8 pt-4 pb-2 rounded">
     <h1 class="text-3xl my-2 font-extrabold tracking-tight text-center text-orange-600">
       {{ title }}
     </h1>
       
     <div class="mt-4 mb-2 mx-4 flex flex-col">
-      <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
+      <div class="overflow-x-auto">
         <div class="inline-block min-w-full py-2 align-middle px-2">
-          <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+          <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 rounded-lg">
             <table class="min-w-full divide-y divide-gray-300">
               <slot />
             </table>
