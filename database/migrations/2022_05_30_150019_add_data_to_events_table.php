c82de0774be2f905948ccfe9369d6abf2bb5e774
Karl Murray <kmurray@foxworldtravel.com>
2022-08-18 23:19:24 -0500

    ...

--- database/migrations/2022_05_30_150019_add_data_to_events_table.php
+++ database/migrations/2022_05_30_150019_add_data_to_events_table.php
@@ -16,8 +16,8 @@ return new class () extends Migration {
             $table->string('name')->after('id')->nullable();
             $table->string('track_name')->after('name')->nullable();
             $table->string('country')->after('track_name')->nullable();
-            $table->string('date_time')->after('country');
-            $table->integer('franchise_id')->after('date_time');
+            $table->string('date_time')->after('country')->nullable();
+            $table->integer('franchise_id')->after('date_time')->nullable();
         });
     }
 
