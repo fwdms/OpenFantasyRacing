04db1be64cc1353fb1c5e89720cf5205b3cadf25
Karl Murray <kmurray@foxworldtravel.com>
2022-09-06 16:49:34 -0500

    Toggles should now be working properly.

--- resources/js/Components/Form/Toggle.vue
+++ resources/js/Components/Form/Toggle.vue
@@ -4,17 +4,19 @@
     class='flex items-center my-4 mx-4'
   >
     <Switch
-      v-model='props.modelValue'
+      v-model='value'
       :class="[
-        modelValue ? 'bg-orange-600' : 'bg-gray-200',
-        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500',
+        value ? 'bg-orange-600' : 'bg-gray-200',
+        'relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full ' +
+         'cursor-pointer transition-colors ease-in-out duration-200 ' +
+         'focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500',
       ]"
-      @update:modelValue='updateValue'
+      @update:modelValue="$emit('update:modelValue', value)"
     >
       <span
         aria-hidden='true'
         :class="[
-          modelValue ? 'translate-x-5' : 'translate-x-0',
+          value ? 'translate-x-5' : 'translate-x-0',
           'pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200',
         ]"
       />
@@ -29,7 +31,7 @@
 </template>
 
 <script setup>
-  import { defineEmits } from 'vue'
+  import { ref, defineProps, onBeforeMount } from 'vue'
   import { Switch, SwitchGroup } from '@headlessui/vue'
   import Label from '@/Components/Form/Label.vue'
   
@@ -39,9 +41,12 @@
     modelValue: Boolean
   })
   
-  const emit = defineEmits(['update:modelValue'])
-  const updateValue = (event) => {
-    emit('update:modelValue', props.modelValue)
-  }
+  onBeforeMount(() => {
+    if (props.modelValue !== undefined) {
+      value.value = props.modelValue
+    }
+  })
+  
+  const value = ref(false)
 
 </script>
