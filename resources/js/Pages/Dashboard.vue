fada85313d425992ffd3adb9b245e8e4240c6495
Karl Murray <kmurray@foxworldtravel.com>
2022-09-09 20:22:13 -0500

    Fixed a broken link

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -16,7 +16,7 @@
             </p>
           </div>
           <div class='mt-4 sm:mt-0 sm:ml-16 sm:flex-none'>
-            <Link href="route('league.create')">
+            <Link :href="route('league.create')">
               <button
                 type='button'
                 class='inline-flex items-center justify-center rounded-md border border-transparent bg-orange-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-700
